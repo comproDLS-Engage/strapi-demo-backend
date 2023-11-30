f56a01069124e774fc266082d87f99c39cd494b8
chauhanvipul25 <vipul.chauhan@comprotechnologies.com>
2023-11-30 16:44:21 +0530

    minor database fix

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,7 +1,3 @@
-// path: ./config/env/production/database.js
-
-const { parse } = require("pg-connection-string");
-
 module.exports = ({ env }) => {
   const host = env("DB_HOST");
   const port = env("DB_PORT");

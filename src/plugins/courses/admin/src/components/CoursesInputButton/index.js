f7ebc1cc9bcd8032c3f096bd21bda39754a07ff7
compro-shridhi <shridhi.aggarwal@comprotechnologies.com>
2023-12-12 12:10:01 +0530

    update CoursesInputButton for custom field

--- src/plugins/courses/admin/src/components/CoursesInputButton/index.js
+++ src/plugins/courses/admin/src/components/CoursesInputButton/index.js
@@ -1,21 +1,39 @@
 import React, { useState } from 'react'
 import { Plus } from '@strapi/icons';
 import { Button } from '@strapi/design-system/Button';
-import CourseModal from '../../components/CourseModal'
+import { Box } from '@strapi/design-system/Box';
+import CourseModal from '../../components/CourseModal';
+import EntityCard from '../../components/EntityCard';
 
-function CoursesInputButton() {
+function CoursesInputButton(props) {
+    const { name, onChange, attribute} = props; 
     const [showModal, setShowModal] = useState(false);
+    const [selectedEntityData, setSelectedEntityData] = useState({
+        selectedCourse: null,
+        entityName: '',
+    });
+
+    console.log("props", props);
+    console.log("props.value",props);
 
     const openModal = () => {
         setShowModal(true);
     }
 
-    const closeModal = () => {
+    const closeModal = (entityData) => {
+        setSelectedEntityData(entityData);
         setShowModal(false);
+        onChange({ target: { name: name, value: entityData, type: attribute.type } });
     }
     return (
         <>
-            <Button onClick={openModal} startIcon={<Plus />}>Link Entity (Course, Event, Resource)</Button>
+            <Box>
+                {selectedEntityData.selectedCourse !== null && selectedEntityData.entityName !== '' ?
+                    <EntityCard selectedEntityData={selectedEntityData} openModal={openModal} setSelectedEntityData={setSelectedEntityData} />
+                    :
+                    <Button onClick={openModal} startIcon={<Plus />}>Link Entity (Course, Event, Resource)</Button>
+                }
+            </Box>
             {showModal && <CourseModal handleClose={closeModal} />}
         </>
     )
